9220bb72a7279bc1f7f5e2b8de4d04b6ac5a7d96
John Cruz <jbcruz0209@gmail.com>
2020-05-09 00:55:34 -0700

    first commit

---
+++ src/client/index.js
@@ -0,0 +1,3 @@
+import './styles/main.scss';
+import '@fortawesome/fontawesome-free/js/all';
+import './views/img/transport.png';
---
+++ src/server/server.js
@@ -0,0 +1,43 @@
+var path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Endpoint for all routes
+let projectData = {};
+
+// BodyParser config
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+app.use(express.static('dist'));
+
+//Get route
+app.get('/', function (req, res) {
+  res.sendFile('dist/index.html');
+});
+
+// Post Route
+app.post('/add', loadInfo);
+
+function loadInfo(req, res) {
+  projectData['depCity'] = req.body.depCity;
+  projectData['arrCity'] = req.body.arrCity;
+  projectData['depDate'] = req.body.depDate;
+  projectData['weather'] = req.body.weather;
+  projectData['daysLeft'] = req.body.daysLeft;
+  res.send(projectData);
+}
+
+// Setup Server
+
+const port = 3030;
+const server = app.listen(port, listening);
+
+function listening() {
+  console.log(`running on localhost: ${port}`);
+  console.log('ENJOY USING TREKER PLANNER!');
+}
